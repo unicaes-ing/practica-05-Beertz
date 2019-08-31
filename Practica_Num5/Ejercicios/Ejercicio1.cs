514e2f67374f1a455e20c89d9556025c3ff79210
Alberto Galdamez <albeerto.galdamezh@gmail.com>
2019-08-30 21:52:59 -0600

    He pasado en limpio mis ejercicios

---
+++ Practica_Num5/Ejercicios/Ejercicio1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Num5
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            fechadate();
+            Console.ReadKey();
+        }
+        static void fechadate()
+        {
+            DateTime hoy = DateTime.Today;
+            String fecha = "31 Diciembre 2019";
+            DateTime cambio = DateTime.Parse(fecha);
+            TimeSpan dif = cambio - hoy;
+            int dias = dif.Days;
+            Console.WriteLine("Faltan {0:N2} dias", dias.ToString());
+
+
+        }
+    }
+
+}
